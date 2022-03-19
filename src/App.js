8c24089aeb815f14785952eceba28d84e2929aa7
Lucia Iramain <luciairamain5@gmail.com>
2022-03-19 16:20:00 -0300

    Se realizo una peticion get para obtener la frase

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, {useState} from "react";
+import './app.css';
+import Frase from "./components/Frase";
 
 function App() {
+
+  const [frase, guardarFrase] = useState({});
+
+  const consultarAPI = async () => {
+    const api = await fetch('https://breaking-bad-quotes.herokuapp.com/v1/quotes');
+    const frase = await api.json();
+    guardarFrase(frase[0]);
+  }
+
   return (
-    <div>
-     <h1>Breaking Bad</h1>
+    <div className="contenedor">
+      <Frase 
+        frase={frase}
+      />
+     <button className="boton" type="submit" onClick={consultarAPI}>Obtener Frase</button>
     </div>
   );
 }
